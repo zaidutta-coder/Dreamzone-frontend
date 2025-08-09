58c0f70b954563cd7aedb61214030dfa3363bbcf
zaidutta-coder <zaidutta@gmail.com>
2025-08-09 23:09:05 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Room from './pages/Room';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/room/:roomId" element={<Room />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
